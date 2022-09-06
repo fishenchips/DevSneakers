dcbcbf5d767868417ea3645d1649183ab70ee615
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-06 19:30:27 +0200

    adding footer code and styling

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <?php wp_footer();
 /* wp_nav_menu(array("theme_location" => "footer-menu"));
  */ ?>
+<div class="footer">
+    <p> &copy; DEVSNEAKERS <?php echo date("Y"); ?>. All rights reserved.</p>
+</div>
 </body>
 
 </html>
