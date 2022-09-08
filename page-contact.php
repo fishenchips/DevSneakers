ae8cc7b9c15ddde8ac02c7e4d763695b0d62331c
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-08 11:49:13 +0200

    creating page contact template and adding pluginform

--- page-contact.php
+++ page-contact.php
@@ -1,14 +1,10 @@
-<?php /* Template Name: Product Template */
+<?php /* Template Name: Contact Template */
 get_header(); ?>
 
 <div class="page-title">
     <h2>
         <?php the_title(); ?>
     </h2>
-</div>
-
-<div class="product-page">
-    <?php the_post_thumbnail("full", array("class" => "product-img")); ?>
 </div>
 
 <!-- the_content(); with styling -->
--- page-start.php
+++ page-start.php
@@ -1,11 +1,10 @@
-<?php get_header(); ?>
+<?php /* Template Name: Start Page Template */
+
+get_header(); ?>
 
 <div class="hero-div">
     <!-- Rendering the Featured Img and giving it a new class -->
     <?php the_post_thumbnail("full", array("class" => "hero-img")); ?>
-    <h2 class="page-header">
-        <?php the_title(); ?>
-    </h2>
 </div>
 
 <?php get_template_part("/template-parts/page-content"); ?>
