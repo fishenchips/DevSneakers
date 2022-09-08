d0c60b3276bccb3b9b04b97ffbb1ec7e28770619
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-08 10:23:13 +0200

    adding and styling title and image on product page

--- page-product.php
+++ page-product.php
@@ -1,5 +1,16 @@
 <?php /* Template Name: Product Template */
-get_header();
+get_header(); ?>
 
+<div class="page-title">
+    <h2>
+        <?php the_title(); ?>
+    </h2>
+</div>
 
-get_footer();
+<div class="product-page">
+    <?php the_post_thumbnail("full", array("class" => "product-img")); ?>
+</div>
+
+<?php the_content(); ?>
+
+<?php get_footer();
--- page.php
+++ page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <div class="hero-div">
+    <!-- Rendering the Featured Img and giving it a new class -->
     <?php the_post_thumbnail("full", array("class" => "hero-img")); ?>
     <h2 class="test">test</h2>
 </div>
