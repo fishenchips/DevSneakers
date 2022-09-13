61667f6daa51e50ebea07841f6ec32b07c3337af
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-13 14:11:18 +0200

    styling single.php

--- single.php
+++ single.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
 
-hej från single
-
+<div class="single-title">
+    <h3>
+        <?php the_title(); ?>
+    </h3>
+</div>
+<div class="single-img">
+    <?php the_post_thumbnail("medium"); ?>
+</div>
+<div class="single-date">
+    <strong>
+        <?php the_time(get_option('date_format')); ?>
+    </strong>
+</div>
+<div class="single-content">
+    <?php the_content(); ?>
+</div>
+<div class="single-link">
+    <a href="/devsneakers/nyheter/">
+        < Tillbaka till nyheter </a>
+</div>
 <?php get_footer(); ?>
